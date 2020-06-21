39f9806170790b8966630551096c4f105fa829e0
Guigxs <guigxs19@gmail.com>
2020-06-21 13:48:39 +0200

    fix submit

--- getStatus.php
+++ getStatus.php
@@ -4,12 +4,12 @@
     if (preg_match('/\bActive: active\b/', $output)){
         $val = "Running";
         echo "<div class='status'>Status: <b style='color: green;'>$val</b></div>";
-        echo "<div class='start'><form method='post'><input type='button' class='btn btn-info' value='Show console' name='showConsole'></input> <input type='button' class='btn btn-danger' value='Stop serve' name='stopServer'></input></form></div>";
+        echo "<div class='start'><form method='post'><input type='submit' class='btn btn-info' value='Show console' name='showConsole'></input> <input type='submit' class='btn btn-danger' value='Stop serve' name='stopServer'></input></form></div>";
     }
     else if (preg_match('/\bActive: inactive\b/', $output)){
         $val = "Not running";
         echo "<div class='status'>Status: <b style='color: red;'>$val</b></div>";
-        echo "<div class='start'><form method='post'><input type='button' class='btn btn-primary' value='Start server' name='startServer'></input></form></div>";
+        echo "<div class='start'><form method='post'><input type='submit' class='btn btn-primary' value='Start server' name='startServer'></input></form></div>";
     }
     else{
         $val = "Error";
