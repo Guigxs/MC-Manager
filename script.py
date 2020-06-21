b13ccb94ed969a16d3b6cced0c81c978fdcf1413
Guigxs <guigxs19@gmail.com>
2020-06-22 00:48:57 +0200

    small button

--- script.py
+++ script.py
@@ -102,12 +102,15 @@ def controller():
 def logs():
     out = sendCommand("cat /home/gui/Lilou/logs/latest.log | aha").decode()
     return '''
-    {}
-    <form method='GET' action='/'>
-        <input class='btn btn-secondary' value='Back to server page' type="submit"></input>
-    </form>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
                     integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    {}
+
+    <form method='GET' action='/'>
+        <input class='btn btn-secondary btn-sm' value='Back to server page' type="submit"></input>
+    </form>
+    
     '''.format(out)
 
 
