ce79fcb3dc3ef829abbe8c4e3ecfea7c801279d3
Guigxs <guigxs19@gmail.com>
2020-06-21 13:09:30 +0200

    fix

--- getStatus.php
+++ getStatus.php
@@ -1,11 +1,11 @@
 <?php
     $output = shell_exec('service MCServ status');
     
-    if (preg_match('/\bActive : active\b/', $output)){
+    if (preg_match('/\bActive: active\b/', $output)){
         $val = "Running";
         echo "<p style='color: green;'>$val</p>";
     }
-    else if (preg_match('/\bActive : inactive\b/', $output)){
+    else if (preg_match('/\bActive: inactive\b/', $output)){
         $val = "Not running";
         echo "<p style='color: red;'>$val</p>";
     }
@@ -13,4 +13,6 @@
         $val = "Error";
         echo "<p style='color: red;'>$val</p>";
     }
+
+    echo "<p>$output</p>"
 ?>
