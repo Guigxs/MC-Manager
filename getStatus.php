dbe892b97d50518a58af1f62a76ee00efcce2d62
Guigxs <guigxs19@gmail.com>
2020-06-21 13:08:24 +0200

    add running/not running

--- getStatus.php
+++ getStatus.php
@@ -1,11 +1,16 @@
 <?php
     $output = shell_exec('service MCServ status');
     
-    if (preg_match('/\bActive\b/', $output)){
-        $val = "Connected";
+    if (preg_match('/\bActive : active\b/', $output)){
+        $val = "Running";
+        echo "<p style='color: green;'>$val</p>";
+    }
+    else if (preg_match('/\bActive : inactive\b/', $output)){
+        $val = "Not running";
+        echo "<p style='color: red;'>$val</p>";
     }
     else{
         $val = "Error";
+        echo "<p style='color: red;'>$val</p>";
     }
-    echo "$val";
 ?>
