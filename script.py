2a931ff22d7cd93c34d1132d5fb0ce66d51d7d2b
Guigxs <guigxs19@gmail.com>
2020-06-22 00:47:49 +0200

    test bootstrap

--- script.py
+++ script.py
@@ -106,6 +106,8 @@ def logs():
     <form method='GET' action='/'>
         <input class='btn btn-secondary' value='Back to server page' type="submit"></input>
     </form>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+                    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     '''.format(out)
 
 
