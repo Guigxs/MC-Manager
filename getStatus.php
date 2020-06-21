590514ffcd45a9c9a83924f9500b8dcceeb3681c
Guigxs <guigxs19@gmail.com>
2020-06-21 13:11:17 +0200

    delete return

--- getStatus.php
+++ getStatus.php
@@ -3,15 +3,15 @@
     
     if (preg_match('/\bActive: active\b/', $output)){
         $val = "Running";
-        echo "<p style='color: green;'>$val</p>";
+        echo "<div style='color: green;'>$val</div>";
     }
     else if (preg_match('/\bActive: inactive\b/', $output)){
         $val = "Not running";
-        echo "<p style='color: red;'>$val</p>";
+        echo "<div style='color: red;'>$val</div>";
     }
     else{
         $val = "Error";
-        echo "<p style='color: red;'>$val</p>";
+        echo "<div style='color: red;'>$val</div>";
     }
 
     echo "<p>$output</p>"
