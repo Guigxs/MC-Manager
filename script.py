ce29d6afae77ebd239b95ae44695910473f74055
Guigxs <guigxs19@gmail.com>
2020-06-23 11:07:55 +0200

    update

--- script.py
+++ script.py
@@ -1,7 +1,6 @@
-from bottle import route, run, template, get, post, request, redirect
+from bottle import route, run, template, get, post, request, redirect, static_file, SimpleTemplate
 import os
 import subprocess
-
 
 host = '0.0.0.0'
 port = 8091
@@ -10,74 +9,9 @@ port = 8091
 def index(name):
     return template('<b>Hello {{name}}</b>!', name=name)
 
-
 @get("/")
 def mcServ():
-
-    return '''
-        <!doctype html>
-        <html lang="en">
-
-            <head>
-                <title>Minecraft manager</title>
-                <!-- Required meta tags -->
-                <meta charset="utf-8">
-                <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-                <!-- Bootstrap CSS -->
-                <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-                    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-                <link rel="stylesheet" href="style.css">
-            </head>
-
-            <body>
-                <div class="navbar navbar-expand-lg navbar-dark bg-dark">
-                    <a class="navbar-brand" href="#">Minecraft Manager</a>
-                </div>
-
-                <div class='status' style='font-size: 50px; text-align : center;'>Status: {}</div>
-
-                <div class="terminal">
-                    <div class="console">
-                    </div>
-
-                    <div class="command">
-                        <form method="post">
-                            <div class="form-group row pl-4 pr-4 pt-2">
-                                
-                                <input type="text" name="commandSend" id="commandSend", class="form-control col-sm-10"></input>
-                                <input type="submit" class='btn btn-secondary btn-sm col-sm-2' value='Send' name='sendCommand'></input>
-                            
-                            </div>
-                            
-                            <div style="text-align : center;">
-
-                                <input type="submit" class='btn btn-primary btn-sm col-sm-3' value='Start server' name='startServer'></input>
-                                <input type="submit" class='btn btn-danger btn-sm col-sm-3' value='Stop server' name='stopServer'></input>
-                                <input type="submit" class='btn btn-info btn-sm col-sm-3' value='Show logs' name='showLogs'></input>
-                            
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            
-
-                <!-- Optional JavaScript -->
-                <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-                <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-                    integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-                    crossorigin="anonymous"></script>
-                <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
-                    integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
-                    crossorigin="anonymous"></script>
-                <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
-                    integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
-                    crossorigin="anonymous"></script>
-            </body>
-
-        </html>
-
-    '''.format(getServiceStatus())
+    return template('templates/main', Status=getServiceStatus(), State="coucou")
 
 @route("/", method='POST')
 def controller():
@@ -118,10 +52,13 @@ def logs():
     
     '''.format(out)
 
+@route("/static/<filename>")
+def server_static(filename):
+    return static_file(filename, root="./static")
+
 
 def getServiceStatus():
-    process = subprocess.Popen("systemctl status MCServ.service", shell = True, stdout=subprocess.PIPE).stdout.read()
-    print(process.decode())
+    process = subprocess.Popen("sudo systemctl status MCServ.service", shell = True, stdout=subprocess.PIPE).stdout.read()
 
     if ("Active: active" in process.decode()):
         return "<b style='color : green;'>Connected</b>"
@@ -132,6 +69,12 @@ def getServiceStatus():
     else :
         return "<b style='color : red;'>Error</b>"
 
+def getStatesInfo():
+    process = subprocess.Popen("sudo systemctl status MCServ.service | grep Active", shell = True, stdout=subprocess.PIPE).stdout.read()
+
+    print(process.decode().split("\n"))
+
+
 def sendServerCommand(command):
     process = subprocess.Popen("screen -S MCServ -X stuff '{}\n'".format(command), shell = True, stdout=subprocess.PIPE).stdout.read()
     return process
@@ -140,4 +83,6 @@ def sendCommand(command):
     process = subprocess.Popen(command, shell = True, stdout=subprocess.PIPE).stdout.read()
     return process
 
-run(host=host, port=port)
+
+if __name__ == "__main__":
+    run(host=host, port=port, reloader=True, debug=True)
---
+++ templates/main.tpl
@@ -0,0 +1,64 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <title>Minecraft manager</title>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+            integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <!-- Personal CSS -->
+        <link rel="stylesheet" href="/static/style.css">
+    </head>
+
+    <body>
+        <div class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <a class="navbar-brand" href="#">Minecraft Manager</a>
+        </div>
+
+        <div class='status'>Status: {{!Status}}</div>
+
+        <div class='state'>{{!State}}</div>
+
+        <div class="terminal">
+            <div class="console">
+            </div>
+
+            <div class="command">
+                <form method="post">
+                    <div class="form-group row pl-4 pr-4 pt-2">
+                        
+                        <input type="text" name="commandSend" id="commandSend", class="form-control col-sm-10" placeholder="Send a command to the server here..."></input>
+                        <input type="submit" class='btn btn-secondary btn-sm col-sm-2' value='Send' name='sendCommand'></input>
+                    
+                    </div>
+                </form>
+            </div>
+        </div>
+        <br>
+        <div class="buttons">
+            <form method="post">
+
+                <input type="submit" class='btn btn-primary col-sm-3' value='Start server' name='startServer'></input>
+                <input type="submit" class='btn btn-danger col-sm-3' value='Stop server' name='stopServer'></input>
+                <input type="submit" class='btn btn-info col-sm-3' value='Show logs' name='showLogs'></input>
+            
+            </form>
+        </div>
+
+        <!-- Optional JavaScript -->
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+            integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+            crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+            integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+            crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+            integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+            crossorigin="anonymous"></script>
+    </body>
+
+</html>
